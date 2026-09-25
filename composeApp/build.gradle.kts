4f3c5f07907dbf52155c7f17e83d7dcb2250372e
Xavante Erickson <xavante.erickson@ericsson.com>
2026-09-25 14:10:21 +0200

    snore

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -48,12 +48,12 @@ kotlin {
 
 android {
     namespace = "com.phantompad.client"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.phantompad.client"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
     }
